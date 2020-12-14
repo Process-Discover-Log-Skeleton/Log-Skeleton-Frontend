785f564c4ad92fc40a630a29d76db8993d430cb9
László Dirks <l.dirks@me.com>
2020-12-14 23:44:07 +0100

    implemented relationship filter and extended handleFilter

---
+++ log-skeleton/src/lib/logic/relationships.js
@@ -0,0 +1,20 @@
+
+export const filterRelationships = (logSkeleton, relationships) => {
+    var filteredLogSkeleton = {}
+
+    // Set the filtered set of activities
+    filteredLogSkeleton['activities'] = logSkeleton['activities']
+    // Set the parameters
+    filteredLogSkeleton['parameters'] = logSkeleton['parameters']
+
+    // Filter the relationships
+    for (let key of relationships) {
+        filteredLogSkeleton[key] =
+            logSkeleton[key]
+    }
+
+    // Set the counter
+    filteredLogSkeleton['counter'] = logSkeleton['counter']
+
+    return filteredLogSkeleton
+}
