8b971f7cd2efe2e597f447044f143767e8e939e2
AveraGE0 <49654038+AveraGE0@users.noreply.github.com>
2020-12-16 10:22:46 +0100

    Add files via upload

--- log-skeleton/src/components/actvivty-filter.js
+++ log-skeleton/src/components/actvivty-filter.js
@@ -1,21 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { useLogSkeleton } from '../lib/api/log-skeleton';
 import styles from "../styles/ActivityFilter.module.css";
 import ListItem from "./list-item";
 
 
-class ActivityFilter extends Component{
+const ActivityFilter = (props) =>{
+    const [items, setItems] = useState(['a1'])//TODO: set items to activities
+
+    return(
+        <div className={styles.scrollComp}>
+            <h1 className={styles.scrollCont}>{props.component_name}</h1>
+            <nav id={'act_nav'}>
+                <ul className={styles.scrollBar}>
+                    {items.map(item=>{
+                        return(
+                            <li className={styles.listItem}>
+                                <ListItem value={item} selectionEvent={props.selectionEvent}></ListItem>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </nav>
+        </div>
+    );
+}
+/*class ActivityFilter extends Component{
     items;
     component_name;
     style;
     logSkeleton;
+    selected_all;
+    deselected_all;
 
     constructor(props) {
         super(props);
         this.style = [styles.scrollComp,styles.scrollCont,styles.scrollBar,styles.listItem];
         //TODO: set items to the states returned by log skeleton
         this.component_name = null;
-        //null = no true = select all false = deselect all
-        this.state ={selection: 0};
+        this.selected_all = false;
+        this.deselected_all = false;
         //this.items = ['a1','a2','a3','a4','a5','a6','a7','a8','a9','a10','a11','a12','a13','a13','a14','a151','a1','a1','a1','a1','a1','a1','a1','a1','a1','a1','a1','this is indeed a very long name and i will see what happens when the name of an activity is just too long',1];
     }
 
@@ -28,15 +51,20 @@ class ActivityFilter extends Component{
     selectionEvent(btn_state, activity){
         throw new Error('SelectionEvent was not implemented!');
     }
-    resetSelection(){
-        if(this.state.selection!=0)
-            this.setState({selection: 0});
+   resetSelection = () =>{
+        this.deselected_all = false;
+        this.selected_all = false;
+       this.forceUpdate();
+   }
+    selectAll = () =>{
+        this.selected_all = true
+        this.deselected_all = false;
+        this.forceUpdate();
     }
-    selectAll(){
-        this.setState({selection: 1});
-    }
-    deselectAll(){
-        this.setState({selection: 2});
+    deselectAll = () =>{
+        this.selected_all = false;
+        this.deselected_all = true;
+        this.forceUpdate();
     }
     render(){
         return(
@@ -55,7 +83,7 @@ class ActivityFilter extends Component{
                         {this.items.map(item=>{
                             return(
                                 <li className={this.style[3]}>
-                                    <ListItem value={item} selection={this.state.selection} selectionReset={()=>this.resetSelection()} selectionEvent={this.selectionEvent}></ListItem>
+                                    <ListItem value={item} select_all={this.selected_all} deselect_all={this.deselected_all} selectionReset={this.resetSelection} selectionEvent={this.selectionEvent}></ListItem>
                                 </li>
                             )
                         })}
@@ -65,6 +93,6 @@ class ActivityFilter extends Component{
         );
 
     }
-}
+}*/
 
 export default ActivityFilter
--- log-skeleton/src/components/forbidden-activities.js
+++ log-skeleton/src/components/forbidden-activities.js
@@ -1,18 +1,10 @@
 import ActivityFilter from "./actvivty-filter";
 
 
-class ForbiddenActivities extends ActivityFilter{
-    constructor(props) {
-        super(props);
-        this.component_name = 'Forbidden Activities';
-        this.items = ['a1','a2','a3','a4','a5','a6','a7','a8','a9','a10','a11','a12','a13','a13','a14','a151','a1','a1','a1','a1','a1','a1','a1','a1','a1','a1','a1','this is indeed a very long name and i will see what happens when the name of an activity is just too long',1];
-        //Binding overridden function
-        this.selectionEvent = this.selectionEvent.bind(this);
-    }
-
-    selectionEvent(btn_state, activity) {
-        //alert('working (forb act), btn state:'+ btn_state + 'act:' + activity);
-    }
+const ForbiddenActivities = () =>{
+    return (
+        <ActivityFilter component_name={'Required Activities'} selectionEvent={(btn)=>{if(btn){/*Insert value into requires activities*/}}}></ActivityFilter>
+    );
 }
 
 
