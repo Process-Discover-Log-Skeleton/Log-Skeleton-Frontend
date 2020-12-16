044fc86d613680cd49e7c4481ea2e618858f21e5
AveraGE0 <49654038+AveraGE0@users.noreply.github.com>
2020-12-16 10:58:40 +0100

    Add files via upload

--- log-skeleton/src/components/list-item.js
+++ log-skeleton/src/components/list-item.js
@@ -1,40 +1,17 @@
-import React, { Component } from 'react';
+import React, {useState} from 'react';
 import style from '../styles/ActivityFilter.module.css';
 
-class ListItem extends Component{
-    constructor(props) {
-        super(props);
-        this.state={btn: false};
-    }
 
-    toggleStyle(){
-        this.setState({btn: !this.state.btn});
-    }
 
-    render() {
-        let btn_style;
-        if(this.props.selection != 0){
-            if(this.props.selection == 1){
-                this.state.btn = true;
-            }
-            else{
-                this.state.btn = false;
-            }
-        }
+const ListItem = (props) =>{
+    const [btn, setBtn] = useState(false);
 
-        if(this.state.btn==true){
-            btn_style=style.btnSelected;
-        }
-        else{
-            btn_style= style.btn;
-        }
+    props.selectionEvent(btn, props.value);
 
-        this.props.selectionEvent(this.state.btn, this.props.value);
+    return(
+        <button className={btn? style.btnSelected: style.btn} onClick={()=>setBtn(!btn)}>{props.value}</button>
+    );
 
-        return(
-            <button className={btn_style} onClick={()=>{this.props.selectionReset(); this.toggleStyle();}}>{this.props.value}</button>
-        );
-    }
 }
 
 
