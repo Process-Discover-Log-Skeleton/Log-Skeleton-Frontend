ee7d9505f14b3e49aef0539b9f7cb227764ae114
Bohne9 <jonah@die-schuellers.de>
2020-12-20 08:10:56 +0100

    🚨 Fix linter.

--- log-skeleton/src/components/relationships.js
+++ log-skeleton/src/components/relationships.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useLogSkeleton } from '../lib/api/log-skeleton'
 import styles from '../styles/SidePanel.module.css'
 
