4da5aabbb06b0550cbba0e06bca1fb36bdaf3e53
Bohne9 <jonah@die-schuellers.de>
2020-12-26 12:10:57 +0100

    ✨ Added multiple arrow markers.

--- log-skeleton/src/lib/common/d3-markers.js
+++ log-skeleton/src/lib/common/d3-markers.js
@@ -1,22 +1,107 @@
+import * as d3 from 'd3'
 
-export const generateMarkers = (svg) => {
+export const generateMarkers = (svg, radius) => {
+
+    // Arrow at the beginning of the edge
+
+    // Filled color
+    svg.append('defs')
+        .append('marker')
+        .attr('id', 'arrowstart-fill')
+        .attr('viewBox', '-0 -2.5 5 5')
+        .attr('refX', -radius / 1.75)
+        .attr('refY', 0)
+        .attr('orient', 'auto')
+        .attr('markerWidth', 6)
+        .attr('markerHeight', 6)
+        .attr('xoverflow', 'visible')
+        .append('svg:path')
+        .attr('d', 'M 0,0 L 5 ,-2.5 L 5, 2.5 L 0, 0')
+        .attr('fill', '#000')
+        .style('stroke','none')
+
+    // Outlined
+    svg.append('defs')
+        .append('marker')
+        .attr('id', 'arrowstart-outline')
+        .attr('viewBox', '-0 -2.5 5 5')
+        .attr('refX', -radius / 1.75)
+        .attr('refY', 0)
+        .attr('orient', 'auto')
+        .attr('markerWidth', 6)
+        .attr('markerHeight', 6)
+        .attr('xoverflow', 'visible')
+        .append('svg:path')
+        .attr('d', 'M 0,0 L 5 ,-2.5 L 5, 2.5 L 0, 0')
+        .attr('fill', '#f4f6f8')
+        .style('stroke','black')
+        .style('stroke-width', '1')
+
+    // Arrow at the end of the arrow
 
     svg.append('defs')
         .append('marker')
-        .attrs({'id':'arrowend',
-            'viewBox':'-0 -2.5 5 5',
-            'refX':-8,
-            'refY':0,
-            'orient':'auto',
-            'markerWidth':9,
-            'markerHeight':9,
-            'xoverflow':'visible'})
+        .attr('id', 'arrowend-fill')
+        .attr('viewBox', '-0 -2.5 5 5')
+        .attr('refX', radius)
+        .attr('refY', 0)
+        .attr('orient', 'auto')
+        .attr('markerWidth', 6)
+        .attr('markerHeight', 6)
+        .attr('xoverflow', 'visible')
         .append('svg:path')
-        .attr('d', 'M 0,-2.5 L 5 ,0 L 0, 2.5')
+        .attr('d', 'M 0,-2.5 L 5 ,0 L 0, 2.5 L 0, -2.5')
         .attr('fill', '#000')
-        .style('stroke','none');
+        .style('stroke','none')
 
+    svg.append('defs')
+        .append('marker')
+        .attr('id', 'arrowend-outline')
+        .attr('viewBox', '-0 -2.5 5 5')
+        .attr('refX', radius)
+        .attr('refY', 0)
+        .attr('orient', 'auto')
+        .attr('markerWidth', 6)
+        .attr('markerHeight', 6)
+        .attr('xoverflow', 'visible')
+        .append('svg:path')
+        .attr('d', 'M 0,-2.5 L 5 ,0 L 0, 2.5 L 0, -2.5')
+        .attr('fill', '#f4f6f8')
+        .style('stroke','black')
+        .style('stroke-width', '1')
 
+    // Rect for the arrow
 
+    svg.append('defs')
+        .append('marker')
+        .attr('id', 'rect-outline')
+        .attr('viewBox', '0 -2.5 5 5')
+        .attr('refX', -radius / 1.75)
+        .attr('refY', 0)
+        .attr('orient', 'auto')
+        .attr('markerWidth', 6)
+        .attr('markerHeight', 6)
+        .attr('xoverflow', 'visible')
+        .append('svg:path')
+        .attr('d', 'M 0, -2.5 L 5, -2.5 L 5, 2.5 L 0, 2.5 L 0, -2.5')
+        .attr('fill', '#f4f6f8')
+        .style('stroke','black')
+        .style('stroke-width', '1')
+
+    svg.append('defs')
+        .append('marker')
+        .attr('id', 'rect-fill')
+        .attr('viewBox', '0 -2.5 5 5')
+        .attr('refX', -radius / 1.75)
+        .attr('refY', 0)
+        .attr('orient', 'auto')
+        .attr('markerWidth', 6)
+        .attr('markerHeight', 6)
+        .attr('xoverflow', 'visible')
+        .append('svg:path')
+        .attr('d', 'M 0, -2.5 L 5, -2.5 L 5, 2.5 L 0, 2.5 L 0, -2.5')
+        .attr('fill', 'black')
+        .style('stroke','black')
+        .style('stroke-width', '1')
 } 
 
