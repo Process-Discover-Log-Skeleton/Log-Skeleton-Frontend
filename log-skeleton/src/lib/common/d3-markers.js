38c851e21349c56a1492557cc173f615cfbea3ec
Bohne9 <jonah@die-schuellers.de>
2020-12-26 13:31:15 +0100

    ✨ Added circlular edge markers.

--- log-skeleton/src/lib/common/d3-markers.js
+++ log-skeleton/src/lib/common/d3-markers.js
@@ -103,5 +103,41 @@ export const generateMarkers = (svg, radius) => {
         .attr('fill', 'black')
         .style('stroke','black')
         .style('stroke-width', '1')
+
+
+    // Circles
+    svg.append('defs')
+        .append('marker')
+        .attr('id', 'circle-fill')
+        .attr('viewBox', '0 -2.5 5 5')
+        .attr('refX', -radius / 1.75)
+        .attr('refY', 0)
+        .attr('orient', 'auto')
+        .attr('markerWidth', 6)
+        .attr('markerHeight', 6)
+        .attr('xoverflow', 'visible')
+        .append('circle')
+        .attr('r', 5)
+        .attr('fill', 'black')
+        .style('stroke','black')
+        .style('stroke-width', '1')
+
+    svg.append('defs')
+        .append('marker')
+        .attr('id', 'circle-outline')
+        .attr('viewBox', '-2.5 -2.5 5 5')
+        .attr('refX', -radius / 1.25)
+        .attr('refY', 0)
+        .attr('orient', 'auto')
+        .attr('markerWidth', 6)
+        .attr('markerHeight', 6)
+        .attr('xoverflow', 'visible')
+        .append('circle')
+        .attr('r', 2)
+        .attr('fill', '#f4f6f8')
+        .style('stroke','black')
+        .style('stroke-width', '0.5')
+
+
 } 
 
